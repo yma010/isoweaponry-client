461ba44ddd38ea0c77cd9b9e0a32b6fe229d2687
Marvin <yma010@ucr.edu>
2020-04-13 15:20:20 -0700

    added reducers for rentals

--- src/reducers/rental_reducer.js
+++ src/reducers/rental_reducer.js
@@ -1,4 +1,18 @@
-import { RECEIVE_ALL_RENTALS} from '../actions/product_actions';
+import { RECEIVE_ALL_RENTALS } from '../actions/product_actions';
 import { merge } from 'lodash';
 
+export default function( state = {}, action) {
+    Object.freeze(state);
+    const newState;
 
+    switch(action.type) {
+        case RECEIVE_ALL_RENTALS:
+            return merge({}, action.rentals)
+        case RECEIVE_RENTAL:
+            newState = merge({}, state);
+            newState[action.rental._id] = action.rental;
+            return newState;
+        default:
+            return state;
+    };
+}

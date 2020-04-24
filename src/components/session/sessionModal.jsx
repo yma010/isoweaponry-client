5db969e6ab9a5d2e42f1063bdfb118ddb99dd3e7
Marvin <yma010@ucr.edu>
2020-04-23 17:18:04 -0700

    stylings

--- src/components/session/sessionModal.jsx
+++ src/components/session/sessionModal.jsx
@@ -15,8 +15,8 @@ export const SessionModal = ({modal, onClose}) => {
         <div className='modal-background' onClick={onClose}>
             <div className="modal-window" onClick={e => e.stopPropagation()}>
                 <section className='modal-contents' >
-                    <h2 className='title'>{modal}</h2>
                     <button className='close-button' aria-label="close" onClick={onClose}> 	<FontAwesomeIcon icon={faTimes}/> </button>
+                    <h2 className='title'>{modal}</h2>
                     {form[modal]}
                 </section>
             </div>

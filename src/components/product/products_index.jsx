cffecab28ee8b96c1e9cc29e98c1e1e36138d959
Marvin <yma010@ucr.edu>
2020-04-24 00:03:50 -0700

    render product listings

--- src/components/product/products_index.jsx
+++ src/components/product/products_index.jsx
@@ -5,17 +5,27 @@ import { useEffect } from 'react';
 
 export function RentalIndex() {
     const dispatch = useDispatch();
-    const rentals = useSelector(state => state.entities.rentals);
-
+    const rentals = useSelector((state) => Object.values(state.entities.rentals))
+    const rentalIdx = rentals.map((rental, index) => {
+        return(
+        <li key={index}>
+            <img src={rental.picture[0]} alt={rental.model}/>
+            <div>{rental.model}</div>
+            <div>{rental.price}</div>
+        </li>
+        )
+    })
     useEffect(() => {
         dispatch(fetchRentals())
-    }, [dispatch])
-    
-    console.log(rentals)
+    }, [dispatch]);
+
+
     return (
         <div>
             This is the Rental Index Page
-        
+            <ul>
+                {rentalIdx}
+            </ul>
         </div>
     )
 }
